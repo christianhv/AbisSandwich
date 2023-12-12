c07c220550328d233813afba542eaab98a9ff759
christian <christian.hv@me.com>
2023-12-12 23:17:57 +0100

    test

--- src/be/abis/sandwich/model/Order.java
+++ src/be/abis/sandwich/model/Order.java
@@ -1,4 +1,10 @@
 package be.abis.sandwich.model;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Order {
+    ArrayList<PersonalOrder> listOfPersonalOrders= new ArrayList<PersonalOrder>();
+    Session session;
+    Date date;
 }
---
+++ src/be/abis/sandwich/test/TestCreateOrders.java
@@ -0,0 +1,7 @@
+package be.abis.sandwich.test;
+
+public class TestCreateOrders {
+    public static void main(String[] args) {
+
+    }
+}
