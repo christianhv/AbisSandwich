c16275f30c033010aeec90768b0cc4709ca544cd
christian.hv@me.com <Emieli&14>
2023-12-13 14:52:08 +0100

    today after reboot

--- src/be/abis/sandwich/test/TestCreateOrders.java
+++ src/be/abis/sandwich/test/TestCreateOrders.java
@@ -1,11 +1,20 @@
 package be.abis.sandwich.test;
 
+import be.abis.sandwich.model.Order;
+import be.abis.sandwich.model.Person;
 import be.abis.sandwich.model.Sandwich;
+import be.abis.sandwich.model.Session;
 import be.abis.sandwich.repository.FileSandwichRepository;
+import be.abis.sandwich.repository.MemoryPersonRepository;
+import be.abis.sandwich.repository.PersonRepository;
 import be.abis.sandwich.repository.SandwichRepository;
+import be.abis.sandwich.theenums.Course;
 import be.abis.sandwich.theenums.SandwichType;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 
 public class TestCreateOrders {
@@ -18,6 +27,13 @@ public class TestCreateOrders {
         Sandwich s = sr.findSandwichByNameandType("BRIE", SandwichType.CHEESE);
 
         System.out.println("found sandwich is:" + s);
+
+        PersonRepository pr = new MemoryPersonRepository();
+        List<Person> allUsers = pr.findAllPersons();
+        LocalDateTime now = LocalDateTime.now();
+        Session sse = new Session(new now, Course.JAVA_ADV);
+
+        Order ord = new Order(sse);
 
 
     }
